8c9f0ea6e78b91f37cf7ae3388e6f38856db2770
Dawid Leszczynski <dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.com>
2022-07-11 22:47:31 +0200

    branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import { Box, Stack } from "@mui/material";
 import { Nav } from "components/molecules/Nav";
 import { MainPage } from "Page/Main";
-import { ThemeProvider } from "styled-components";
-import { useDarkMode } from "hooks/useDarkMode";
-import { lightTheme, darkTheme } from "const/Theme";
 import { GlobalStyles } from "Styles/GlobalStyle";
 import ProviderContextTheme from "context/ThmeProviderContex";
 
 function App() {
-  // const [theme, toggleTheme] = useDarkMode();
-
-  // const themeMode = theme ? lightTheme : darkTheme;
-
   return (
     <ProviderContextTheme>
       <GlobalStyles />
