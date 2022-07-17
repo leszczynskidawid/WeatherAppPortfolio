a8f4c202e03ec2c19f765016666328a3599d9e1a
Dawid Leszczynski <dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.com>
2022-07-17 17:19:26 +0200

    change styled

--- src/components/atoms/Input/style.js
+++ src/components/atoms/Input/style.js
@@ -2,7 +2,9 @@ import { InputBase } from "@mui/material";
 import styled from "styled-components";
 
 export const Search = styled.div`
-  background-color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.containerLigth};
+  border-color: ${(props) => props.theme.text};
+  border: 1px solid;
   border-radius: 9px;
   padding: 0 10px;
   width: 70%;
